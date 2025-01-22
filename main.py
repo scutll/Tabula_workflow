d04dd4950f5b2be15ab084d289ee854232f622ad
mxl <mxlscut153@163.com>
2025-01-22 17:16:42 +0800

    some repair

--- main.py
+++ main.py
@@ -26,26 +26,32 @@ def add_to_send_list(data):
 confirm: 删除对应pending
 其他: 将请求放入执行列表并发送确认消息
 '''
-async def recv(text,websocket):
-    data=json.loads(text)
+async def recv(data,websocket):
+
+    print('data: ',data)
+
     #前端确认收到消息，后端删除对应pending队列
     if data['type'].lower()=='confirm':
+        print('confirm msg received')
         requestId=data['requestId']
         if requestId in pending_requests:
             pending_requests[requestId]['time_out'].cancel() #取消超时任务
+            print('deleting pening list:')
             del pending_requests[requestId]
 
 
-    elif data['type'] !='confirm': #somthing else
+    elif data["type"] !='confirm': #somthing else
         #后端接收消息并发送确认请求
+        print('msg received! sending confirm:')
         confirm={
-            type:'confirm',
-            data:None,
-            requestId:data['requestId']
+            "type":"confirm",
+            "data":None,
+            "requestId":data['requestId']
         }
         await websocket.send(json.dumps(confirm))
+        print('adding to request_list')
         request_list.insert(0,data)
-        print(data)
+        print(request_list)
 
 
 '''
@@ -61,6 +67,7 @@ def Get_requestId():
 发送前添加入待确认(pending_reuqests)列表并在超时或被取消(confirmed)后删除，若未得到确认则添加到发送列表重新发送
 '''
 async def send(request,websocket):
+    print('sending request')
     requestId=request['requestId']
     await websocket.send(json.dumps(request))
 
@@ -89,20 +96,31 @@ async def handle(websocket):
 
     async def recv_request():
         async for text in websocket:
-            await recv(text,websocket)
+            print('msg received:')
+            text=text.strip("'")
+            try:
+                data=json.loads(text)
+                print(data)
+            except json.JSONDecodeError as e:
+                print(e)
+            asyncio.create_task(recv(data,websocket))
 
     async def send_request():
         while True:
             if request_to_send:
                 request=request_to_send.pop(0)
-                asyncio.create_task(send(request,websocket))
-            
+                await send(request,websocket)
             await asyncio.sleep(0.1)
 
     await asyncio.gather(recv_request(),send_request())
 
 
+
+
+
+
 async def main():
+    print("server running")
     async with websockets.serve(handle,'0.0.0.0',5200):
         await asyncio.Future()
 
